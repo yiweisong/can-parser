cb7198264a5842f8cc22edc16b30e44571223623
Song Yi Wei <ywsong@aceinna.com>
2026-01-21 11:00:59 +0800

    fix: update the artifacts build

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import runpy
+
+
+SRC_PATH = os.path.join(os.getcwd(), 'src')
+EXECUTOR_PATH = os.path.join(SRC_PATH, 'aceinna', 'executor.py')
+
+sys.path.append('./src')
+runpy.run_path(EXECUTOR_PATH, run_name='__main__')
